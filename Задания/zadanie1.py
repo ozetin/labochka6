31559e4c6a39e76ae72634cfe0e910d812f7be6f
student-09-525 <student-09-525@ncfu.net>
2025-12-10 11:58:38 +0300

    Изменено задание 1

--- Задания/zadanie1.py
+++ Задания/zadanie1.py
@@ -7,15 +7,22 @@ if __name__ == '__main__':
     marshruti = []
     def add(start, end, number):
         global marshruti
+        imeetsya = 1
 
-        marshrut = {
-            'start': start,
-            'end': end,
-            'number': number
-        }
+        for i in marshruti:
+            if i['number'] == number:
+                imeetsya = 0
+                print("Маршрут с таким номером уже есть")
+                break
+        if imeetsya:
+            marshrut = {
+                'start': start,
+                'end': end,
+                'number': number
+            }
 
-        marshruti.append(marshrut)
-        marshruti = sorted(marshruti, key=lambda k: k['number'])
+            marshruti.append(marshrut)
+            marshruti = sorted(marshruti, key=lambda k: k['number'])
 
     def vivod(number):
         schet = 0
@@ -29,13 +36,28 @@ if __name__ == '__main__':
         if schet == 0:
             print("Таких маршрутов не найдено")
 
+    def list():
+        global marshruti
+        if marshruti:
+            for i in marshruti:
+                print(f"Номер: {i['number']}\n"
+                      f"Начало: {i['start']}\n"
+                      f"Конец: {i['end']}\n")
+        else:
+            print("Маршрутов нет")
+
+
     while True:
         chto = input("Что сделать?\n "
                      "vivod - вывести маршрут по номеру\n "
-                     "add - добавить маршрут\n")
+                     "add - добавить маршрут\n"
+                     "list - вывести список маршрутов\n")
 
         if chto == "vivod":
             vivod(int(input("Введите номер маршрута: ")))
+
+        elif chto == "list":
+            list()
 
         elif chto == "add":
             start = input("Введите начальный пункт маршрута: ")
